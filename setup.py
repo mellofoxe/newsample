9337c50307a4e04994a3a7486c51d0ecbf8861b1
Amy <mellofoxe@gmail.com>
2018-05-27 21:57:36 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='newsample',
+      version='0.1',
+      description='python sample module',
+      url='http://github.com/jaslGH/newsample',
+      author='Test',
+      author_email='jaslGH@gmail.com',
+      license='',
+      packages=['newsample'],
+      zip_safe=False)
+
